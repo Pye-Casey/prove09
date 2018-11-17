dfa503ba2d584f27dab4f99413fbcf07c9f865d3
Pye-Casey <caseypye@byui.edu>
2018-11-17 12:13:02 -0700

    added mail prices

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const mail = require('./mail.js');
+var app = express();
+
+app.use(express.static(__dirname + '/public')); 
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/shipping', mail.computeShipping)
   .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
